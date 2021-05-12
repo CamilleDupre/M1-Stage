66d9a14f4750627205b6248d1f7f393094ddbc52
CamilleDupre <47179915+CamilleDupre@users.noreply.github.com>
2021-05-12 11:47:53 +0200

    sphère follow hands

--- Assets/Script/Network_Player.cs
+++ Assets/Script/Network_Player.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using Valve.VR;
 using Photon.Pun;
 
-
 public class Network_Player : MonoBehaviour
 
 {
@@ -13,15 +12,25 @@ public class Network_Player : MonoBehaviour
     public Transform leftHand;
     public Transform rightHand;
 
-    private PhotonView photonView;
+    private GameObject headset;
+    private GameObject right;
+    private GameObject left;
 
-    public Transform Right;
-    public Transform Left;
+    private PhotonView photonView;
+    public SteamVR_Input_Sources inputSource;
+    public SteamVR_Input_Sources inputSource2;
+    public SteamVR_Action_Pose poseAction = SteamVR_Input.GetAction<SteamVR_Action_Pose>("Pose");
+
 
     // Start is called before the first frame update
     void Start()
     {
         photonView = GetComponent<PhotonView>();
+
+
+        headset = GameObject.Find("/[CameraRig]/Camera");
+        right = GameObject.Find("/[CameraRig]/Controller (right)");
+        left = GameObject.Find("/[CameraRig]/Controller (left)");
     }
 
     // Update is called once per frame
@@ -29,25 +38,47 @@ public class Network_Player : MonoBehaviour
     {
         if (photonView.IsMine)
         {
-            Vector3 headPosition = SteamVR_Render.Top().head.position;
-            Transform cameraRig = SteamVR_Render.Top().origin;
+            //Vector3 headPosition = SteamVR_Render.Top().head.position;
+           // Transform cameraRig = SteamVR_Render.Top().origin;
 
-          
+
             //leftHand.gameObject.SetActive(false);
-           // rightHand.gameObject.SetActive(false);
+            // rightHand.gameObject.SetActive(false);
             //head.gameObject.SetActive(false);
 
-            MapPosition(head, cameraRig);
-            MapPosition(rightHand, Right);
-            MapPosition(leftHand, Left);
-
+            /*
+            MapPosition(head, XRNode.Head);
+            MapPosition(rightHand, XRNode.RightHand);
+            MapPosition(leftHand, XRNode.LeftHand);
+            */
+            MapPosition();
         }
        
     }
 
-    void MapPosition(Transform target, Transform cameraRig)
+    //void MapPosition(Transform target, XRNode node)
+    void MapPosition()
     {
-        target.position = cameraRig.position;
-        target.rotation = cameraRig.rotation;
+        /*  InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
+          InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
+          target.position = position;
+          target.rotation = rotation;
+        */
+
+        // m_pose.inputSource
+        // head.position = SteamVR_Render.Top().origin.position;
+        // head.rotation = SteamVR_Render.Top().origin.rotation;
+        //rightHand.position = poseAction[inputSource].localPosition;
+        //rightHand.rotation = poseAction[inputSource].localRotation;
+
+        leftHand.position = left.transform.position;
+        leftHand.rotation = left.transform.rotation;
+
+        rightHand.position = right.transform.position;
+        rightHand.rotation = right.transform.rotation;
+
+        
+        head.position = headset.transform.position;
+        head.rotation = headset.transform.rotation;
     }
 }
