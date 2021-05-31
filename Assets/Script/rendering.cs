5c55a6511b20c28bebec42dfbae8928822c24bce
CamilleDupre <47179915+CamilleDupre@users.noreply.github.com>
2021-05-31 16:00:00 +0200

    Update rendering.cs

--- Assets/Script/rendering.cs
+++ Assets/Script/rendering.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class rendering : MonoBehaviourPunCallbacks //, MonoBehaviourPun
 {
@@ -65,13 +66,12 @@ public class rendering : MonoBehaviourPunCallbacks //, MonoBehaviourPun
         //StartCoroutine(waiter());
     }
 
-
-    public override void OnJoinedRoom()
+    public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        if (!PhotonNetwork.IsMasterClient) { 
-        Debug.Log("Joined a room !!");
-        base.OnJoinedRoom();
+       // Debug.Log("new player");
+        //base.OnPlayerEnteredRoom(newPlayer);
     //}
+
     //public override void OnCreatedRoom() { 
         
         
@@ -83,25 +83,25 @@ public class rendering : MonoBehaviourPunCallbacks //, MonoBehaviourPun
         Debug.Log("Creation carte " + PhotonNetwork.IsMasterClient);
 
         int nbcard = textures.Length;
-            for (int i = 0; i < nbcard; i++)
+        for (int i = 0 ; i < nbcard ; i++)
+        {
+            if (i < nbcard / 3)
             {
-                if (i < nbcard / 3)
-                {
-                    MyCard c = new MyCard((Texture2D)textures[i], MurL, i);
-                    c.pv.RPC("LoadCard", Photon.Pun.RpcTarget.All, c.pv.ViewID, MurL.GetComponent<PhotonView>().ViewID, i);//GetComponent<PhotonView>().ViewID);
-                }
-                else if (i < 2 * nbcard / 3)
-                {
-                    MyCard c = new MyCard((Texture2D)textures[i], MurB, i - nbcard / 3);
-                    c.pv.RPC("LoadCard", Photon.Pun.RpcTarget.All, c.pv.ViewID, MurB.GetComponent<PhotonView>().ViewID, i - nbcard / 3);
-                }
-                else
-                {
-                    MyCard c = new MyCard((Texture2D)textures[i], MurR, i - 2 * nbcard / 3);
-                    c.pv.RPC("LoadCard", Photon.Pun.RpcTarget.All, c.pv.ViewID, MurR.GetComponent<PhotonView>().ViewID, i - 2 * nbcard / 3);
-                }
-
+              MyCard c = new MyCard((Texture2D)textures[i], MurL, i);
+                c.pv.RPC("LoadCard", Photon.Pun.RpcTarget.All, c.pv.ViewID, MurL.GetComponent<PhotonView>().ViewID , i);//GetComponent<PhotonView>().ViewID);
             }
+            else if (i < 2* nbcard / 3)
+            {
+               MyCard c = new MyCard((Texture2D)textures[i], MurB, i - nbcard / 3);
+                c.pv.RPC("LoadCard", Photon.Pun.RpcTarget.All, c.pv.ViewID, MurB.GetComponent<PhotonView>().ViewID, i - nbcard / 3);
+            }
+            else 
+            {
+              MyCard c = new MyCard((Texture2D)textures[i], MurR, i - 2 * nbcard / 3);
+                c.pv.RPC("LoadCard", Photon.Pun.RpcTarget.All, c.pv.ViewID, MurR.GetComponent<PhotonView>().ViewID, i - 2 * nbcard / 3);
+            }
+        
+        
         }
     }
 
